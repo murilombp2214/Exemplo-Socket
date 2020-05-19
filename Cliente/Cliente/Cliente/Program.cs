25fa6dbdf28470819e706de0159ad422026724c3
murilo.peixoto <murilo.peixoto@N1001721>
2020-05-19 02:21:02 -0300

    initial commit

---
+++ Cliente/Cliente/Cliente/Program.cs
@@ -0,0 +1,351 @@
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+
+    public class Coroutine
+    {
+
+        public Func<string, string> FuncaoHash { get; private set; }
+
+        /// <summary>
+        /// Caminho do arquivo
+        /// </summary>
+
+        public string PathFile { get; set; }
+
+        /// <summary>
+        /// Construtor Defualt
+        /// </summary>
+
+        public Coroutine(Func<string, string> funcHash)
+
+        {
+            FuncaoHash = funcHash;
+        }
+
+        /// <summary>
+        /// Cria o objeto ja ocm o nome do arquivo
+        /// </summary>
+        /// <param name="funcHash">Funcao hash</param>
+        /// <param name="pathFile">arquivo</param>
+
+        public Coroutine(Func<string, string> funcHash, string pathFile)
+
+        {
+
+            PathFile = pathFile;
+
+            FuncaoHash = funcHash;
+
+        }
+
+
+        /// <summary>
+        /// Ler o arquivo usando yeld return
+        /// </summary>
+        /// <returns></returns>
+
+        private IEnumerable<string> LerArquivoCoroutine()
+
+        {
+
+            using var st = new StreamReader(path: PathFile);
+            string word = string.Empty;
+            while ((word = st.ReadLine()) != null)
+            {
+                yield return FuncaoHash(word);
+            }
+
+        }
+
+        /// <summary>
+        /// Ler o arquivo usando yeld return assincrono
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<string> LerArquivoCoroutineAsync()
+        {
+            var st = new StreamReader(path: PathFile);
+            string word = string.Empty;
+            while (word != null)
+            {
+                word = st.ReadLine();
+
+                if (!string.IsNullOrEmpty(word))
+                {
+                    yield return FuncaoHash(word);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processa dos dados do arquivo usando a coroutine
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> BusquePorCoroutine()
+
+        {
+            foreach (var item in LerArquivoCoroutine())
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// Processa dos dados do arquivo usando a coroutine assincrona
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> BusquePorCoroutineAsync()
+        {
+            foreach (var item in LerArquivoCoroutineAsync())
+            {
+                yield return item;
+            }
+
+        }
+
+    }
+
+
+
+
+    class Program
+    {
+        static class TypesMinion
+        {
+            public static string PossoProcessar = "PossoProcessar";
+            public static string Encontrei = "Encontrei";
+            public static string Encerre = "Encerre";
+        }
+        static bool encontrouHash = false;
+        private static Coroutine objCoroutine;
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "";
+        }
+
+
+        private static void ProcesseMinion(string msgParaServidor, bool pedirTarefa)
+        {
+            try
+            {
+                if (encontrouHash)
+                {
+                    return;
+                }
+
+                string enderecoIp = GetLocalIPAddress();
+                int porta = 1024 + 1;
+                int tamanhoMax = 99999;
+
+
+                //Conecta com servidor
+                TcpClient client = new TcpClient();
+                client.Connect(enderecoIp, porta);
+
+                //Envia mensagem para o servidor
+                NetworkStream stream = client.GetStream();
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(msgParaServidor);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Mensagem Enviada: {msgParaServidor}");
+
+
+                if (pedirTarefa)
+                {
+                    //Mensagem Recebida servidor
+                    byte[] bytesRecebidosServer = new byte[tamanhoMax];
+                    int qtdBytesRecebidosServer = stream.Read(bytesRecebidosServer, 0, bytesRecebidosServer.Length);
+                    string responseServidor = System.Text.Encoding.ASCII.GetString(bytesRecebidosServer, 0, qtdBytesRecebidosServer);
+
+                    string[] vetRespServ = responseServidor.Split(';');
+                    string palavraAhSerBuscada = vetRespServ[1];
+                    string list = vetRespServ[0];
+                    List<string> wordList = JsonConvert.DeserializeObject<List<string>>(list);
+
+                    //A Lista do servidor acabou ou achou a wordlist
+                    if (vetRespServ.Length > 3 && vetRespServ[2] == TypesMinion.Encerre)
+                    {
+                        return;
+                    }
+
+                    if (wordList != null && wordList.Count > 0)
+                    {
+                        //Processa a wordList
+                        string senha = ProcesseWordList(wordList, palavraAhSerBuscada);
+
+                        if (encontrouHash)  //se alguem encontrou não processa mais
+                        {
+                            return;
+                        }
+
+                        if (senha == string.Empty) //senão encontrar a word pede mais uma tarefa ao servidor
+                        {
+                            Task.Run(() => ProcesseMinion(TypesMinion.PossoProcessar + ";" + Environment.ProcessorCount.ToString(), true));
+                        }
+                        else //se encontrar uma tarefa avisa o servidor que encontrou
+                        {
+                            Task.Run(() =>
+                            {
+                                ProcesseMinion(TypesMinion.Encontrei + ";" + senha, false);
+                                encontrouHash = true;
+                            });
+                        }
+                    }
+                }
+
+
+                //fecha as conexões
+                stream.Close();
+                client.Close();
+                Console.ReadKey();
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("Houve um erro no procesameto do miniom");
+            }
+
+        }
+
+        public static string HashSHA256(string value)
+
+        {
+
+            using SHA256 objSha = SHA256.Create();
+
+            byte[] bytes = objSha.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+            StringBuilder Retorno = new StringBuilder();
+
+            for (int i = 0; i < bytes.Length; i++)
+
+                Retorno.Append(bytes[i].ToString());
+
+            return Retorno.ToString().ToUpper();
+
+        }
+
+
+        private static void CrieArquivoDeWordList(string diretorio, string arquivo, List<string> wordList)
+        {
+            if (!Directory.Exists(diretorio))
+            {
+                Directory.CreateDirectory(diretorio);
+
+            }
+
+            if (!File.Exists(Path.Combine(diretorio, arquivo)))
+            {
+                File.Create(Path.Combine(diretorio, arquivo)).Close();
+            }
+            else
+            {
+                File.Delete(Path.Combine(diretorio, arquivo));
+                File.Create(Path.Combine(diretorio, arquivo)).Close();
+            }
+
+            var streamCript = new StreamWriter(Path.Combine(diretorio, arquivo));
+
+            foreach (var item in wordList)
+            {
+                streamCript.WriteLine(item);
+            }
+
+
+            streamCript.Close();
+
+
+        }
+
+        private static string ProcesseWordList(List<string> wordList, string palavraAhSerBuscada)
+        {
+
+            string diretorio = "E:/Faculdade/SD/Socket/ArquivosLixo";
+            string arquivo = $"{Guid.NewGuid()}.txt";
+
+            CrieArquivoDeWordList(diretorio, arquivo, wordList);
+
+            ParallelOptions po = new ParallelOptions();
+            po.CancellationToken = new CancellationTokenSource().Token;
+            po.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+            objCoroutine = new Coroutine((value) => HashSHA256(value), Path.Combine(diretorio, arquivo));
+
+            palavraAhSerBuscada = HashSHA256(palavraAhSerBuscada);
+
+
+            string senhaEncontrada = string.Empty;
+
+            if(totalmeenteEmParalelo)
+            {
+                Parallel.ForEach(objCoroutine.BusquePorCoroutineAsync(), po, (result, state) =>
+                {
+
+                    try
+                    {
+                        if (!string.IsNullOrEmpty(result))
+                        {
+                            string hash = HashSHA256(result);
+                            Console.WriteLine($"hash => {result} => busca => {palavraAhSerBuscada}");
+                            if (hash == palavraAhSerBuscada)
+                            {
+                                senhaEncontrada = hash;
+                            }
+                        }
+                    }
+
+                    catch (Exception erro)
+                    {
+
+                        Console.WriteLine("Houve um erro ao processar essa wordList");
+                    }
+
+                });
+            }
+            else
+            {
+                foreach (var item in objCoroutine.BusquePorCoroutineAsync())
+                {
+                    if (item == palavraAhSerBuscada)
+                    {
+                        senhaEncontrada = item;
+                        break;
+                    }
+                }
+
+            }
+
+
+            return senhaEncontrada;
+
+        }
+        private static bool totalmeenteEmParalelo = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 's' para executar em paralelo.");
+            totalmeenteEmParalelo = Console.ReadLine() == "s";
+            string msg = TypesMinion.PossoProcessar + ";" + Environment.ProcessorCount.ToString();
+            ProcesseMinion(msg, true);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Servidor/Servidor/Program.cs
@@ -0,0 +1,178 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    static class TypesMinion
+    {
+        public static string PossoProcessar = "PossoProcessar";
+        public static string Encontrei = "Encontrei";
+        public static string Encerre = "Encerre";
+    }
+
+
+    class Program
+    {
+        //atributos 
+        private static string enderecoIp = GetLocalIPAddress();
+        private static int porta = 1024 + 1;
+
+        //wordList
+        private static List<string> wordList = new List<string>();
+
+        //index que esta a wordlist
+        private static int indexAtualWordList = 0;
+
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+
+        class Teste
+        {
+            public int Idade { get; set; }
+        }
+        private static void InitServer(string palavraAhSerBuscada)
+        {
+            try
+            {
+
+                //CRIA SERVIDOR
+                var endpoint = new IPEndPoint(IPAddress.Parse(enderecoIp), porta);
+                var servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                servidor.Bind(endpoint);
+
+                bool ehParaPararOhLaco = false;
+                Socket clienteSock = null;
+                while (!ehParaPararOhLaco)
+                {
+                    servidor.Listen(100);
+                    clienteSock = servidor.Accept(); //aguarda uma chamada de um minion
+
+                    clienteSock.ReceiveBufferSize = 16384; //tamanho do buffer
+
+                    byte[] dadosCliente = new byte[1024]; //dados recebidos do cliente
+                    int tamanhoBytesRecebidos = clienteSock.Receive(dadosCliente, dadosCliente.Length, 0); //tamanho dos bytes recebidos
+
+                    string response = Encoding.ASCII.GetString(dadosCliente, 0, tamanhoBytesRecebidos); //mensagem do cliente
+
+                    string[] elementos = response.Split(';'); // 0 => TypeMinion, 1 => qtdNucleos
+
+                    if (elementos.Length != 2)
+                    {
+                        clienteSock.Send(Encoding.ASCII.GetBytes("A quantidade de parametros para essa requisição deve ser igual a 2, e o memso é concatenado em uma string dividido por ponto e virgula"));
+
+                    }
+                    else
+                    {
+                        if (elementos[0] == TypesMinion.PossoProcessar)
+                        {
+                            var listaParaSerProcessada = GetElementosWordList(int.Parse(elementos[1]) * 300);
+
+                            if (listaParaSerProcessada.Count == 0)
+                            {
+                                ehParaPararOhLaco = true;
+                                byte[] bytesEnviarClinte = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(listaParaSerProcessada) + ";" + palavraAhSerBuscada + ";" + TypesMinion.Encerre);
+                                clienteSock.Send(bytesEnviarClinte);
+                            }
+                            else
+                            {
+                                byte[] bytesEnviarClinte = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(listaParaSerProcessada) + ";" + palavraAhSerBuscada);
+                                clienteSock.Send(bytesEnviarClinte);
+                            }
+                        }
+                        else //encontrou a senha na wordlist
+                        {
+                            string senhaDescriptografada = response.Split(';')[1];
+                            Console.WriteLine($"Senha encontrada, senha: {senhaDescriptografada} ");
+                            ehParaPararOhLaco = true;
+                        }
+                    }
+
+
+                    clienteSock.Close();
+                }
+
+
+
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Erro ao executar o servidor, erro com a mensagem \n {erro.Message}");
+            }
+        }
+
+
+        private static List<string> GetElementosWordList(int qtdElementos)
+        {
+            qtdElementos = indexAtualWordList + qtdElementos;
+            var listWord = new List<string>();
+            while (indexAtualWordList < wordList.Count && indexAtualWordList < qtdElementos)
+            {
+                listWord.Add(wordList[indexAtualWordList++]);
+            }
+
+            return listWord;
+
+
+        }
+
+
+
+        public static void GerarDadosArquivos()
+        {
+            string diretorio = "E:/Faculdade/SD/Socket/Arquivos/";
+            string arquivo = "WordList.txt";
+
+            if (!Directory.Exists(diretorio))
+            {
+                Directory.CreateDirectory(diretorio);
+
+            }
+
+            if (!File.Exists(Path.Combine(diretorio, arquivo)))
+            {
+                File.Create(Path.Combine(diretorio, arquivo)).Close();
+            }
+            else
+            {
+                File.Delete(Path.Combine(diretorio, arquivo));
+                File.Create(Path.Combine(diretorio, arquivo)).Close();
+            }
+
+            for (decimal i = 0; i  < 100; ++i)
+            {
+                wordList.Add(Guid.NewGuid().ToString());
+            }
+            wordList.Add("MURILO");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => GerarDadosArquivos());
+            string palavraAhSerBuscada = "MURILO";
+            Console.WriteLine("*** Servidor ***");
+            Task.Run(() => InitServer(palavraAhSerBuscada));
+            Console.ReadKey();
+        }
+    }
+}
